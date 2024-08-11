dcf645e211fb9ff546cfe7e1270fc97532eb4c3a
AYTC <aytacalir@gmail.com>
2024-08-11 12:02:07 +0300

    7

--- app.js
+++ app.js
@@ -49,17 +49,81 @@
 //? 3.soru
 // Bir nesne ve bir anahtar-değer çifti alan, ve bu anahtar-değer çiftini nesneye ekleyerek yeni bir nesne döndüren bir fonksiyon yazın.
 
-function nesneyiDeğiştir(nesne,anahtar,değer){
-    return {...nesne,[anahtar]:değer};
+// function nesneyiDeğiştir(nesne,anahtar,değer){
+//     return {...nesne,[anahtar]:değer};
     
-}
-console.log(nesneyiDeğiştir({a:1,b:2},'c',3));  // Çıktı:{a: 1, b: 2, c: 3}
+// }
+// console.log(nesneyiDeğiştir({a:1,b:2},'c',3));  // Çıktı:{a: 1, b: 2, c: 3}
 
 
 //? 4.soru
 // İç içe diziler alan ve bu diziyi tek katmanlı bir diziye dönüştüren bir fonksiyon yazın.
 
-function düzleştir(dizi){
+// function düzleştir(dizi) {
+//     return dizi.reduce((sonuç, öğe) => 
+//         sonuç.concat(Array.isArray(öğe) ? düzleştir(öğe) : öğe), []);
+// }
+
+// console.log(düzleştir([1, [2, 3], [4, [5]]])); // Çıktı: [1, 2, 3, 4, 5]
+
+//? 5.soru
+
+// İki nesne alan ve bu iki nesnenin özelliklerini birleştirerek yeni bir nesne döndüren bir fonksiyon yazın.
+
+// function nesneleriBirleştir(nesne1,nesne2){
     
+//     let newNesne = {...nesne1,...nesne2}
+//     return newNesne
+
+    
+// }
+// console.log(nesneleriBirleştir({a:1},{b:2}));// Çıktı:{a: 1, b: 2}
+
+
+//? 6.soru
+// Bir nesne ve bir özellik anahtarı alan ve bu özelliği nesneden çıkartarak yeni bir nesne döndüren bir fonksiyon yazın.
+
+
+// function özelliğiKaldır(nesne,kaldırılacakAnahtar){
+//     delete nesne[kaldırılacakAnahtar]
+//     return nesne
+    
+// }
+// console.log(özelliğiKaldır({a:1,b:2,c:3},'b'));// Çıkt ı: {a: 1, c: 3}
+
+//? 7.soru
+// Birden fazla nesne alan ve tüm özellikleri birleştirerek yeni bir nesne döndüren bir fonksiyon yazın. Aynı anahtara sahip özellikler varsa, son nesnenin değeri kullanılsın.
+
+
+
+// function özellikleriBirleştir(...nesneler) {
+//     return Object.assign({}, ...nesneler);
+// }
+
+// console.log(özellikleriBirleştir({a: 1}, {b: 2}, {a: 3}, {b: 5})); // Çıktı: {a: 3, b: 2}
+
+
+//? 8.soru
+// Bir nesne dizisi alan ve bu dizinin derin bir kopyasını döndüren bir fonksiyon yazın
+
+// function diziKopyala(dizi){
+//     return dizi.map(öğe => JSON.parse(JSON.stringify(öğe)));
+//     }
+//     const orijinal=[{a:1},{b:2}];
+//     const kopya=diziKopyala(orijinal);
+//     kopya[0].a=99;
+//     console.log(orijinal[0].a);// Çıktı: 1
+
+
+
+
+    function diziKopyala(dizi) {
+        return dizi.map(öğe => ({ ...öğe }));
     }
-console.log(düzleştir([1,[2,3],[4,[5]]]));// Çıktı: [1,2, 3, 4, 5]
+    
+    const orijinal = [{a: 1}, {b: 2}];
+    const kopya = diziKopyala(orijinal);
+    kopya[0].a = 99;
+    
+    console.log(orijinal[0].a); // Çıktı: 1
+    
